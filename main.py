b6425e325a0a7d2c205b74e3d77b13c673cd8cd9
star14ms <68639278+star14ms@users.noreply.github.com>
2020-08-08 18:02:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.insert(0, r'C:\Users\danal\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.8_qbz5n2kfra8p0\LocalCache\local-packages\Python38\site-packages')
-
 import requests
 from bs4 import BeautifulSoup
 
